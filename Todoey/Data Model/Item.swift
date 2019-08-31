022a684af3eaffd02e569a7d6a4992b92f51fa1f
Amit Ashkenazy <amit471@gmail.com>
2019-09-01 01:09:53 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Amit Ashkenazi on 31/08/2019.
+//  Copyright © 2019 Amit Ashkenazy. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
